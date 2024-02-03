f4f90eafe13f05bfe03deb26ee1c9750dd149da0
Adam Natale <adam.natale@mail.utoronto.ca>
2024-02-03 11:08:33 -0500

    Pull team members stats from API

--- src/App.js
+++ src/App.js
@@ -11,31 +11,13 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-) {
-  return { name, calories, fat, carbs, protein };
-}
+const teamsData = require("./config");
 
-const adamRows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+const adamRows = [];
+let adamPoints = 0;
 
-const haleyRows = [
-  createData('Toast', 159, 6.0, 24, 4.0),
-  createData('Eggs', 237, 9.0, 37, 4.3),
-  createData('Butter Chicken', 262, 16.0, 24, 6.0),
-  createData('Coffee', 305, 3.7, 67, 4.3),
-  createData('Oatmeal Cookies', 356, 16.0, 49, 3.9),
-];
+const haleyRows = [];
+let haleyPoints = 0;
 
 function App() {
   const [data, setData] = React.useState(null);
@@ -43,7 +25,33 @@ function App() {
   React.useEffect(() => {
     fetch("/api")
       .then((res) => res.json())
-      .then((data) => setData(data.message));
+      .then((data) => {
+        console.log(data);
+        console.log(teamsData);
+        teamsData.teamsData.adamTeam.forEach((player) => {
+          console.log(player);
+          let obj = data.find(o => o.name === player);
+          console.log(obj);
+          adamPoints += parseInt(obj.points);
+          adamRows.push({
+            name: player,
+            gamesPlayed: obj.gamesPlayed,
+            points: obj.points
+          });
+        });
+
+        teamsData.teamsData.haleyTeam.forEach((player) => {
+          console.log(player);
+          let obj = data.find(o => o.name === player);
+          haleyPoints += parseInt(obj.points);
+          haleyRows.push({
+            name: player,
+            gamesPlayed: obj.gamesPlayed,
+            points: obj.points
+          });
+        });
+        setData(data.message);
+      });
   }, []);
 
   // <img src={logo} className="App-logo" alt="logo" />
@@ -51,7 +59,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>{!data ? "Loading..." : data}</p>
         <Grid container spacing={2}>
           <Grid item xs={6}>
             Adams!
@@ -60,21 +67,19 @@ function App() {
             Haleys!
           </Grid>
           <Grid item xs={6}>
-            Adams Points!
+            {adamPoints}
           </Grid>
           <Grid item xs={6}>
-            Haleys Points!
+            {haleyPoints}
           </Grid>
           <Grid item xs={6}>
             <TableContainer component={Paper}>
               <Table sx={{ minWidth: 650 }} aria-label="simple table">
                 <TableHead>
                   <TableRow>
-                    <TableCell>Dessert (100g serving)</TableCell>
-                    <TableCell align="right">Calories</TableCell>
-                    <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                    <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                    <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                    <TableCell>Name</TableCell>
+                    <TableCell align="right">Games Played</TableCell>
+                    <TableCell align="right">Points</TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
@@ -86,10 +91,8 @@ function App() {
                       <TableCell component="th" scope="row">
                         {row.name}
                       </TableCell>
-                      <TableCell align="right">{row.calories}</TableCell>
-                      <TableCell align="right">{row.fat}</TableCell>
-                      <TableCell align="right">{row.carbs}</TableCell>
-                      <TableCell align="right">{row.protein}</TableCell>
+                      <TableCell align="right">{row.gamesPlayed}</TableCell>
+                      <TableCell align="right">{row.points}</TableCell>
                     </TableRow>
                   ))}
                 </TableBody>
@@ -101,11 +104,9 @@ function App() {
               <Table sx={{ minWidth: 650 }} aria-label="simple table">
                 <TableHead>
                   <TableRow>
-                    <TableCell>Dessert (100g serving)</TableCell>
-                    <TableCell align="right">Calories</TableCell>
-                    <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                    <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                    <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                    <TableCell>Name</TableCell>
+                    <TableCell align="right">Games Played</TableCell>
+                    <TableCell align="right">Points</TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
@@ -117,10 +118,8 @@ function App() {
                       <TableCell component="th" scope="row">
                         {row.name}
                       </TableCell>
-                      <TableCell align="right">{row.calories}</TableCell>
-                      <TableCell align="right">{row.fat}</TableCell>
-                      <TableCell align="right">{row.carbs}</TableCell>
-                      <TableCell align="right">{row.protein}</TableCell>
+                      <TableCell align="right">{row.gamesPlayed}</TableCell>
+                      <TableCell align="right">{row.points}</TableCell>
                     </TableRow>
                   ))}
                 </TableBody>
---
+++ src/config.js
@@ -0,0 +1,14 @@
+const teamsData = {
+    adamTeam: [
+        "Adam Natale",
+        "Doug Baird",
+        "Alan Zuckerman"
+    ],
+    haleyTeam: [
+        "Haley Jackson",
+        "Shane Jackson",
+        "Jason McKay"
+    ]
+}
+
+module.exports = { teamsData };
